b96b6e73be6db98416f2e90e8256964e24fbb349
degawl <devongawley@gmail.com>
2019-06-06 14:47:25 +0300

    Add user account opening and closing

--- includes/form_handlers/login_handler.php
+++ includes/form_handlers/login_handler.php
@@ -3,18 +3,24 @@
 if (isset($_POST['login_button'])) {
     $email = filter_var($_POST['log_email'], FILTER_SANITIZE_EMAIL);
 
-    $_SESSION['login_email'] = $email;
+    $_SESSION['log_email'] = $email;
     $password = md5($_POST['log_password']);
 
-    $database_check_query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-    $database_check = mysqli_query($con, $database_check_query);
-    $check_login = mysqli_num_rows($database_check);
+    $database_check_query_string = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+    $database_check_query = mysqli_query($con, $database_check_query_string);
+    $check_login_query = mysqli_num_rows($database_check_query);
 
-    if ($check_login == 1) {
-        $row = mysqli_fetch_array($database_check);
+    if($check_login_query == 1) {
+        $row = mysqli_fetch_array($database_check_query);
         $username = $row['username'];
-
+        
+        $user_closed = $row['user_closed'];
+        echo $user_closed;
+        if ($user_closed == 'yes'){
+            $reopen_account = mysqli_query($con,"UPDATE users SET user_closed='no' WHERE email='$email'");   
+        }
         $_SESSION['username'] = $username;
+        
         header("Location: index.php");
         exit();
     } else {
